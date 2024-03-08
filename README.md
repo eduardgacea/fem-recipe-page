c8073d8aed1efb22b2f92ebbf8c30cc805f9df46
eduardgacea <eduardgacea@gmail.com>
2024-03-08 21:54:37 +0200

    update readme with live version of the application

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Recipe page design exercise
 
 Simple styling using CSS modules, no preprocessor, no CSS-in-JS, no tailwind
+
+Live version available [here](https://fem-recipe-page-design.netlify.app/)
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <header className={styles.header}>
-            <img src="image-omelette.jpeg" />
+            <img src="image-omelette.jpeg" alt="presentation image" />
         </header>
     );
 }
